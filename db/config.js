bf26b4335893718fd44a31e203f1129314b0bf29
JCDiaz1201 <jcdiaz1201@gmail.com>
2019-12-02 16:30:07 -0600

    First commit

---
+++ db/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'productInfo',
+    port: 3306
+}
---
+++ db/db.js
@@ -0,0 +1,10 @@
+let mysql = require('mysql');
+let mysqlConfig = require('config.js');
+
+let connection = mysql.createConnection(mysqlConfig);
+
+
+
+
+
+
