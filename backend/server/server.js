fe3a80d821c74ebc8ffcd500f0cfc45149a5c539
JCDiaz1201 <jcdiaz1201@gmail.com>
2019-12-04 16:53:22 -0600

    Restructured overall directory for ease of deployment to EC2 on AWS

---
+++ backend/server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../db/db');
+const path = require('path')
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+
+app.get('/getAllProducts', (req, res) => {
+    db.getAllProducts()
+    .then((data) => {
+        res.send(data);
+        res.end();
+    })
+    .catch((err) => {
+        res.send(err);
+        res.end();
+    })
+});
+
+app.post('/getSingleProduct', (req, res) => {
+    let reqItem = req.body.selectedItemId;
+    // console.log(reqItem);
+    // res.send(reqItem);
+    // res.end();
+
+    db.getSingleProduct(reqItem)
+    .then((data) => {
+        res.send(data);
+        res.end();
+    })
+    .catch((err) => {
+        res.send(err);
+        res.end();
+    })
+})
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
