2d7f1d5779e7a8e48a977a194838a546153ec031
JCDiaz1201 <jcdiaz1201@gmail.com>
2019-12-06 17:07:42 -0600

    Updated App function for deployment

---
+++ server/db/db.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'productInfo',
+    port: 3306
+}
+
+// module.exports = {
+//     host: 'product-infor-db3.ckazxowg68vw.us-east-2.rds.amazonaws.com',
+//     user: 'admin',
+//     password: 'FUaws1201',
+//     database: 'productInfo',
+//     port: 3306
+// }
+
+// mysql -h product-infor-db3.ckazxowg68vw.us-east-2.rds.amazonaws.com -P 3306 -u admin -p
